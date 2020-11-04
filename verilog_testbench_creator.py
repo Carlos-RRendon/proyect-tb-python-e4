3892982deb8f3a08227b8bf79716dd846a7d676a
Carlos R. Rendon <carlos.ramirezrn@outlook.com>
2020-11-04 09:54:58 -0600

    Is capital

--- verilog_testbench_creator.py
+++ verilog_testbench_creator.py
@@ -11,18 +11,22 @@ class tesbench_creator:
     def abrir_archivo(self):
         f = open(self.path, 'r')
         content = f.read()
-        patterns = '^[A-Z]'
+        patterns = 'module'
         word = str()
 
-        for x in content:
-            is_capital = re.search(patterns, x)
+        is_capital = re.search(patterns, content)
+        print(is_capital)
 
-            if is_capital:
-                word = word + " " + x
-            else:
-                word = word + x
 
-        #print(word)
+
+
+
+if __name__ == "__main__":
+
+    creator = tesbench_creator("design.sv")
+    creator.abrir_archivo()
+
+
 
 
 
