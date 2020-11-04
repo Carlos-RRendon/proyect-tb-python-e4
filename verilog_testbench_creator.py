9a6c7aa9733707e123741d59338c59057be8683c
Carlos R. Rendon <carlos.ramirezrn@outlook.com>
2020-11-04 09:32:14 -0600

    Creacion de la clase

--- verilog_testbench_creator.py
+++ verilog_testbench_creator.py
@@ -1,3 +1,46 @@
-print("hola mundo")
-f = str("FPM")
-print ("Hola", f)
+#!/usr/bin/env python
+import re
+
+class tesbench_creator:
+
+    def __init__(self,path):
+
+        self.path = path
+
+
+    def abrir_archivo(self):
+        f = open(self.path, 'r')
+        content = f.read()
+        patterns = '^[A-Z]'
+        word = str()
+
+        for x in content:
+            is_capital = re.search(patterns, x)
+
+            if is_capital:
+                word = word + " " + x
+            else:
+                word = word + x
+
+        #print(word)
+
+
+
+'''
+#def text_match (text) :
+patterns = '^[A-Z]'
+
+f = open('design.sv', 'r')
+content = f.read()
+
+word = str()
+
+for x in content :
+    is_capital = re.search(patterns, x)
+
+    if is_capital :
+        word = word + " " + x
+    else :
+        word = word + x
+
+print(word)'''
