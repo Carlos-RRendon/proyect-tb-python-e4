d816b21a005460f7154f2e29ae40cedcb67a0044
Carlos R. Rendon <carlos.ramirezrn@outlook.com>
2020-11-04 12:22:19 -0600

    Archivo conjunto

--- tb_creator.py
+++ tb_creator.py
@@ -6,20 +6,55 @@ class tesbench_creator:
 
     def __init__(self, path):
         self.path = path
+        self.content = ""
 
     def abrir_archivo(self):
         f = open(self.path, 'r')
         content = f.read()
 
-        pattern_module = re.search('module\s(.*)', content)
-        string_module = pattern_module.group(1)
+        '''pattern_module = re.search('module\s(.*)', content)
+        string_module = pattern_module.group(1)'''
 
-        pattern_module_2 = re.search("(.*)\s*\((.*?)\)", string_module)
-        module_name = pattern_module_2.group(1)
-        ports = pattern_module_2.group(2)
+        # Quita los saltos de linea
+        content = content.replace("\n", " ")
+        f.close()
 
+        # Actualiza el atributo content
+        self.content = self.content + content
+
+    def extract_info(self):
+
+        import re
+        self.abrir_archivo()
+
+        pattern = "module|input|output|inout"
+        data_clean = []
+
+        self.content = self.content.split(";")
+
+        for line in self.content:
+            if re.search(pattern, line):
+                data_clean.append(line)
+
+
+        return data_clean
+
+    def find_module(self):
+        content = self.extract_info()
+
+        for string_module in content:
+
+            ''''''
+            pattern_module = re.search('module\s(.*)', content)
+            string_module = pattern_module.group(1)
+
+
+            pattern_module_2 = re.search("(.*)\s*\((.*?)\)", string_module)
+            module_name = pattern_module_2.group(1)
+            ports = pattern_module_2.group(2)
+            print(ports)'''
 
 
 if __name__ == "__main__":
     creator = tesbench_creator("design.sv")
-    creator.abrir_archivo()
+    creator.find_module()
